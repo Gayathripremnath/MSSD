737a999d85b73bfac4fbec96e6cfe8ec02cb8b43
Gayathripremnath <gayathripremnath7@gmail.com>
2026-06-12 12:09:41 +0530

    commit

--- gallery/views.py
+++ gallery/views.py
@@ -3,14 +3,19 @@ from rest_framework import generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import GalleryImage, AlbumImage
 from .serializers import GalleryImageSerializer, AlbumImageSerializer
+from .pagination import GalleryPagination
+
 
 class GalleryListView(generics.ListAPIView):
     queryset = GalleryImage.objects.all().order_by('-created_at')
     serializer_class = GalleryImageSerializer
+    pagination_class = GalleryPagination
+
 
 class GalleryDetailView(generics.RetrieveAPIView):
     queryset = GalleryImage.objects.all()
     serializer_class = GalleryImageSerializer
+
 
 class AlbumListView(generics.ListAPIView):
     queryset = AlbumImage.objects.all()
