cf0901a8e8ed6e101c4e1192dbfed678147d3630
Gayathripremnath <gayathripremnath7@gmail.com>
2026-06-16 12:26:17 +0530

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -132,11 +132,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-CORS_ALLOW_ALL_ORIGINS = True
-
 CORS_ALLOWED_ORIGINS = [
-    'https://mss-public-s.vercel.app',
+    "https://mss-public-s.vercel.app",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOW_METHODS = [
     "DELETE",
@@ -157,4 +157,3 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-CORS_ALLOW_ALL_ORIGINS = True
