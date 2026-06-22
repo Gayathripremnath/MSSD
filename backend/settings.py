c6078bb967fa6666aeb8a8b8d4ede49b6ad9f98d
Gayathripremnath <gayathripremnath7@gmail.com>
2026-06-22 16:47:25 +0530

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -133,11 +133,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://mss-public-s.vercel.app",
     "http://localhost:5173",
+    "http://localhost:5174",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
